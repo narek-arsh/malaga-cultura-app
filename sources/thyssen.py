c5f38c1e07cbe87da39f5d7b51151e8a557b07f8
narek-arsh <narekikol@gmail.com>
2025-09-02 17:24:42 +0200

    Update thyssen.py

--- sources/thyssen.py
+++ sources/thyssen.py
@@ -5,6 +5,7 @@ from mc_utils.model import Event
 def scrape_thyssen(exhibitions_url: str, activities_url: str, tickets_url: Optional[str], institution_id: str, institution_name: str) -> List[Event]:
     evs: List[Event] = []
     if exhibitions_url:
+        # en su web suele aparecer "/exposiciones" o "/exposiciones-temporales"
         evs += scrape_from_listing(exhibitions_url, "exhibition", institution_name, institution_id, tickets_url, hint="/exposiciones")
     if activities_url:
         evs += scrape_from_listing(activities_url, "activity", institution_name, institution_id, tickets_url, hint="/actividades")
